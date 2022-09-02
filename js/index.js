e3c3b27369800cb36ba5525633a30dc10be55ef8
golamrabbani93 <golamrabbani9373@gmail.com>
2022-09-02 23:28:09 +0600

    Display complete category item

--- js/index.js
+++ js/index.js
@@ -10,3 +10,17 @@ const loadCategory = async () => {
 	}
 };
 loadCategory();
+
+const displayCategory = (datas) => {
+	// *find category Container
+	const categoryContainer = document.getElementById('category-box');
+	datas.forEach((data) => {
+		// *create li Element
+		const li = document.createElement('li');
+		li.innerHTML = `
+         <a onclick="loadNewscategory('${data.category_id}')" class="nav-link mx-3" aria-current="page" href="#">${data.category_name}</a>
+        `;
+
+		categoryContainer.appendChild(li);
+	});
+};

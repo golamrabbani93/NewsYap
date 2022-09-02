0d046ff635643f7fbbd1128df08fcf5bf54eee1a
golamrabbani93 <golamrabbani9373@gmail.com>
2022-09-03 00:03:23 +0600

    Display News Item length complete

--- js/index.js
+++ js/index.js
@@ -19,22 +19,33 @@ const displayCategory = (datas) => {
 		// *create li Element
 		const li = document.createElement('li');
 		li.innerHTML = `
-         <a onclick="loadNewscategory('${data.category_id}')" class="nav-link mx-3" aria-current="page" href="#">${data.category_name}</a>
+         <a onclick="loadNewscategory('${data.category_id}','${data.category_name}')" class="nav-link mx-3" aria-current="page" href="#">${data.category_name}</a>
         `;
 
 		categoryContainer.appendChild(li);
 	});
 };
 // *load All News category by categories id
-const loadNewscategory = async (id) => {
+const loadNewscategory = async (id, name) => {
 	const url = ` https://openapi.programming-hero.com/api/news/category/${id}`;
 
 	try {
 		const res = await fetch(url);
 		const datas = await res.json();
-		console.log(datas.data);
+		displayNewsItem(datas.data, name);
 	} catch (error) {
 		console.log(error);
 	}
 };
-loadNewscategory('01');
+loadNewscategory('01', 'Breaking News');
+
+const displayNewsItem = (datas, name) => {
+	// *Display News Item Length
+	const lengthContainer = document.getElementById('category-length');
+	lengthContainer.textContent = '';
+	const lengthText = document.createElement('h3');
+	lengthText.innerHTML = `
+        <span class="color-primary">${name}:</span> ${datas.length} News Found
+    `;
+	lengthContainer.appendChild(lengthText);
+};

7486f9af2acd6c0fd66f4a8e317826b59180d804
golamrabbani93 <golamrabbani9373@gmail.com>
2022-09-03 17:21:59 +0600

    blog.html file create and setup all page Location

---
+++ js/location.js
@@ -0,0 +1,15 @@
+// *page location setup
+
+// *home Page
+document.getElementById('home').addEventListener('click', function () {
+	window.location.href = 'index.html';
+});
+
+// * News Page
+document.getElementById('news').addEventListener('click', function () {
+	window.location.href = 'index.html';
+});
+// * Blog Page
+document.getElementById('blog').addEventListener('click', function () {
+	window.location.href = 'blog.html';
+});
